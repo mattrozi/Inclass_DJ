f800289d0cfd15ce29259d02c300e8bf51d11e8b
Matt Rozinskas <matt_rozinskas1@baylor.edu>
2022-04-27 16:03:57 -0500

    Changes made at 4/27

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import TopicForm
 
 from .models import Topic
 
-# Create your views here.
+# Create your views here.  Get and posts requests in html ***FINAL EXAM QUESTION***
 
 def index(request):
     return render(request,"MainApp/index.html")
@@ -15,4 +17,28 @@ def topics(request):
     context={'topics':topics}
 
     return render(request,'MainApp/topics.html',context)
-    
+
+def topic(request,topic_id):
+    topic=Topic.objects.get(id=topic_id)
+    entries=topic.entry_set.order_by('-date_added')
+
+    context={'topic':topic,'entries':entries}  #It's a dictionary, final exam  question
+
+    return render(request,'MainApp/topic.html',context)
+
+
+def new_topic(request):
+    if request.method!='POST':
+        form=TopicForm
+    else:
+        form=TopicForm(data=request.POST)
+
+        if form.is_valid():
+            new_topic=form.save()
+
+            return redirect('MainApp:topics')
+
+    context={'form':form}
+
+    return render(request,'MainApp/new_topic.html',context)
+            
