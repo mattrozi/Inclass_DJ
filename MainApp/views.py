863ff8e01295fb1cef974a1e7a1da3d5c0bf0cff
Matt Rozinskas <matt_rozinskas1@baylor.edu>
2022-05-04 15:48:58 -0500

    Changes on May the 4th

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,32 +1,43 @@
+import http
 from django.shortcuts import render,redirect
 
 from .forms import EntryForm, TopicForm
 
 from .models import Topic,Entry
 
+from django.contrib.auth.decorators import login_required
+
+from django.http import Http404
+
 # Create your views here.  Get and posts requests in html ***FINAL EXAM QUESTION***
 
 def index(request):
     return render(request,"MainApp/index.html")
 
-
+@login_required
 def topics(request):
-    topics=Topic.objects.all()
+    topics=Topic.objects.filter(owner=request.user).order_by('-date_added-')
 
 
     context={'topics':topics}
 
     return render(request,'MainApp/topics.html',context)
 
+@login_required
 def topic(request,topic_id):
     topic=Topic.objects.get(id=topic_id)
+
+    if topic.owner!=request.user:
+        raise Http404
+
+
     entries=topic.entry_set.order_by('-date_added')
 
     context={'topic':topic,'entries':entries}  #It's a dictionary, final exam  question
 
     return render(request,'MainApp/topic.html',context)
 
-
+@login_required
 def new_topic(request):
     if request.method!='POST':
         form=TopicForm
@@ -34,7 +45,9 @@ def new_topic(request):
         form=TopicForm(data=request.POST)
 
         if form.is_valid():
-            new_topic=form.save()
+            new_topic=form.save(commit=False)
+            new_topic.owner=request.user
+            new_topic.save()
 
             return redirect('MainApp:topics')
 
@@ -43,9 +56,12 @@ def new_topic(request):
     return render(request,'MainApp/new_topic.html',context)
     
 
-
+@login_required
 def new_entry(request,topic_id):
     topic=Topic.objects.get(id=topic_id)
+
+    if topic.owner!=request.user:
+        raise Http404
 
     if request.method != 'POST':
         form=EntryForm()
@@ -63,10 +79,13 @@ def new_entry(request,topic_id):
 
     return render(request,'MainApp/new_entry.html',context)
 
+@login_required
 def edit_entry(request,entry_id):
     entry=Entry.objects.get(id=entry_id)
     topic=entry.topic
 
+    if topic.owner!=request.user:
+        raise Http404
 
     if request.method != 'POST':
         form=EntryForm(instance=entry)
